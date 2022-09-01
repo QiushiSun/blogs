afd97f1af13b440c1c9bc7516d9ead089cdb04ca
QiushiSun <thomasqiushi@icloud.com>
2022-09-01 14:51:53 +0800

    Site updated: 2022-09-01 14:51:53

---
+++ js/gallery.js
@@ -0,0 +1,52 @@
+// author by removef
+// https://removeif.github.io/
+$(function () { //获取处理友链数据
+    $.getJSON("../json_data/gallery.json", function (data) {
+        // var data0 = data[0];
+        $('.links-content').html("");
+        // 随机排序过滤失效的
+        let notValid = data.filter((item, a, b) => item.valid == 0);
+        data = data.filter((item, a, b) => item.valid != 0).sort(function (a, b) {
+            return 1;
+        });
+        $('.links-content').append("<div class='friend-title-item'><br>Selected Honors & Awards & Publications<br><br><hr></div>");
+        $.each(data, function (i, e) {
+            var html = "<div class=\"friend-card-item\">";
+            if (e.src == undefined) {
+                html += "    <img class=\"ava\" src=\"/img/links/nopic.jpg\" title=\"Default Pics\">";
+            } else {
+                html += "    <img class=\"ava\" src=\"" + e.src + "\">";
+            }
+            html +=
+                "<div class='text-desc' title=\""+e.desc+"\">    Name：<a href=\"" + e.url + "\" target=\"_blank\">" + e.name + "</a>" +
+                "    <br>Time：" + e.date +
+                "<br>Description：" + e.desc + "</div>" +
+                "    </div>";
+
+            $('.links-content').append(html);
+        });
+
+        // 过期的
+        if (notValid.length > 0) {
+            $('.links-content').append("<div class='friend-title-item'><br>异常的大佬们<br><br><hr></div></div>");
+            $.each(notValid, function (i, e) {
+                var html = "<div class=\"friend-card-item\">";
+                html += "    <img class=\"ava\" src=\"/img/links/nopic.jpg\" title=\"图片链接不可用，使用的默认图片\">";
+                html +=
+                    "<div class='text-desc' title=\""+e.desc+"\">    网址：<a href=\"" + e.url + "\" target=\"_blank\">" + e.name + "</a>" +
+                    "    <br>访问时间：" + e.stopTime +
+                    "<br>简介：" + e.desc + "</div>" +
+                    "    </div>";
+
+                $('.links-content').append(html);
+            })
+        }
+        $('.links-content').append("</div>");
+    }
+
+
+
+
+
+    )
+});
